b1bfa61637117c15a9e9c67a51408c0b5d83f839
Rexx Alfaro <121982791+x3rD1@users.noreply.github.com>
2025-04-07 18:50:27 -0700

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended", "prettier"],
+  },
+
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+]);
