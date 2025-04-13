7b265336c6cc1f168cb699017f46420e27371876
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-04-12 20:13:33 -0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devtool: "eval-source-map",
+  devtool: false,
   devServer: {
     watchFiles: ["./src/index.html"],
   },
