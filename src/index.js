9df8067596b64946948bdf56a699fcfec0c7763c
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-04-12 19:31:52 -0700

    Updated all weather information

--- src/index.js
+++ src/index.js
@@ -1,55 +1,110 @@
 import "./style.css";
 
 // Z2VNGZRYMTFGL73BGN3UPCULB
+const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const conversion = (temp) => {
+  let fahrenheit = false;
+
+  return !fahrenheit ? `${(((temp - 32) * 5) / 9).toFixed(1)}°C` : `${temp}°F`;
+};
+
 const loc = document.querySelector("#loc");
 const btn = document.querySelector("button");
+const form = document.querySelector("form");
 
-btn.addEventListener("click", () => {
-  const capitalize = loc.value[0].toUpperCase() + loc.value.slice(1);
-  getWeatherFrom(capitalize);
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  getWeatherFrom(loc.value);
+  form.reset();
 });
 
 async function getWeatherFrom(location) {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/
-      ${location}?key=Z2VNGZRYMTFGL73BGN3UPCULB`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=Z2VNGZRYMTFGL73BGN3UPCULB`,
       { mode: "cors" },
     );
 
     if (!response.ok) {
-      throw new Error("Invalid input");
+      throw new Error("City not found!");
     }
 
     const data = await response.json();
     const weatherData = getWeatherData(data);
-
+    console.log(data);
     displayWeatherData(weatherData);
+    displayForecastData(data);
   } catch (err) {
-    alert(err);
+    console.error("Error fetching weather data:", err);
+    alert(err.message || "An unknown error occurred");
   }
 }
 
 function getWeatherData(data) {
+  const address = data.resolvedAddress;
+  const resolvedAddress = address.split(/[\s,]+/);
+  let result;
+  if (resolvedAddress.length === 1) {
+    result = resolvedAddress[0];
+  } else {
+    result = `${resolvedAddress[0]}, ${resolvedAddress[resolvedAddress.length - 1]}`;
+  }
   const weatherData = {
-    address: data.resolvedAddress.split(",")[0].trim(),
+    address: result,
     temp: data.currentConditions.temp,
     conditions: data.currentConditions.conditions,
+    feelsLike: data.currentConditions.feelslike,
+    humidity: data.currentConditions.humidity,
+    windSpeed: data.currentConditions.windspeed,
+    pressure: data.currentConditions.pressure,
   };
   return weatherData;
 }
 
-function displayWeatherData({ address, temp, conditions }) {
+function displayWeatherData(data) {
   // const container = document.querySelector('.container');
-  const loc = document.querySelector(".address");
-  const temperature = document.querySelector(".temp");
+  const city = document.querySelector(".city");
+  const temperature = document.querySelector(".temperature");
   const condition = document.querySelector(".condition");
+  const felt = document.querySelector(".feel");
+  const humidity = document.querySelector(".humidity-percentage");
+  const wind = document.querySelector(".wind-speed-value");
+  const feels = document.querySelector(".feels-like-value");
+  const pressure = document.querySelector(".pressure-value");
 
-  let fahrenheit = false;
+  city.textContent = data.address;
+  condition.textContent = data.conditions;
+  felt.textContent = `Feels like ${conversion(data.feelsLike)}`;
+  temperature.textContent = conversion(data.temp);
+  humidity.textContent = `${data.humidity}%`;
+  wind.textContent = `${data.windSpeed} km/h`;
+  feels.textContent = conversion(data.feelsLike);
+  pressure.textContent = `${data.pressure} hPa`;
+}
 
-  loc.textContent = address;
-  temperature.textContent = !fahrenheit
-    ? `${(((temp - 32) * 5) / 9).toFixed(1)}°C`
-    : `${temp}°F`;
-  condition.textContent = conditions;
+function displayForecastData(data) {
+  const days = data.days;
+  for (const [index, day] of days.entries()) {
+    if (index === 0) continue;
+    if (index > 5) break;
+
+    const date = new Date(days[index + 1].datetime);
+    const dayCondition = document.querySelector(`.day-card${index}-condition`);
+    const weekDay = document.querySelector(`.day-card${index}`);
+    const feelsMax = document.querySelector(`.feelsMax${index}`);
+    const feelsMin = document.querySelector(`.feelsMin${index}`);
+
+    dayCondition.textContent = day.conditions;
+    weekDay.textContent = weekDays[date.getDay()];
+    feelsMax.textContent = conversion(day.feelslikemax);
+    feelsMin.textContent = conversion(day.feelslikemin);
+  }
 }
